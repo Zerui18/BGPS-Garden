f15f4e19d09154c9ed22539457bb481824f57886
TheoLeeCJ <theodorelcj@gmail.com>
2021-07-03 21:37:29 +0800

    added overlays (sections hardcoded for now)

--- lib/src/screens/map/map.dart
+++ lib/src/screens/map/map.dart
@@ -1,5 +1,18 @@
 import 'package:bgps_garden/src/library.dart';
 
+extension GlobalKeyExtension on GlobalKey {
+  Rect get globalPaintBounds {
+    final renderObject = currentContext?.findRenderObject();
+    var translation = renderObject?.getTransformTo(null)?.getTranslation();
+    if (translation != null && renderObject.paintBounds != null) {
+      return renderObject.paintBounds
+          .shift(Offset(translation.x, translation.y));
+    } else {
+      return null;
+    }
+  }
+}
+
 class MapWidget extends StatefulWidget {
   const MapWidget({Key key}) : super(key: key);
 
@@ -34,25 +47,165 @@ class _MapWidgetState extends State<MapWidget> with WidgetsBindingObserver {
     super.dispose();
   }
 
+  List<GlobalKey> containerKeys = [
+    GlobalKey(),
+    GlobalKey(),
+    GlobalKey(),
+    GlobalKey(),
+    GlobalKey(),
+    GlobalKey(),
+    GlobalKey(),
+    GlobalKey(),
+  ];
+  List<double> topStart = List.filled(8, 0);
+  List<double> leftStart = List.filled(8, 0);
+  List<dynamic> locationActions = [];
+  List<dynamic> pinLocations = [];
+  List<String> sectionNames = [
+    "Cactus Garden",
+    "Butterfly Garden",
+    "Native Garden",
+    "Fruit Tree Corner",
+    "Vegetable Plot",
+    "Ornamental Plants",
+  ];
+
   @override
   Widget build(BuildContext context) {
+    pinLocations = [
+      [110.0, 63.0],
+      [30.0, 80.0],
+      [150.0, 80.0],
+      [140.0, 100.0],
+      [310.0, 40.0],
+      [320.0, 70.0],
+    ];
+
+    void updatePositions() {
+      setState(() {
+        for (int i = 0; i < pinLocations.length; i++) {
+          topStart[i] = containerKeys[i].globalPaintBounds.top;
+          leftStart[i] = containerKeys[i].globalPaintBounds.left;
+        }
+      });
+    }
+
+    List<Widget> stackChildren = [
+      Positioned(
+        top: MediaQuery.of(context).size.height * 0.05,
+        child: Container(
+          height: MediaQuery.of(context).size.height * 0.65,
+          width: MediaQuery.of(context).size.width,
+          decoration: BoxDecoration(
+            image: DecorationImage(
+              fit: BoxFit.contain,
+              image: AssetImage('assets/images/map.png'),
+              alignment: Alignment.topCenter
+            ),
+            borderRadius: BorderRadius.all(Radius.circular(20))
+          ),
+        ),
+      ),
+    ];
+
+    List<Widget> pins = [];
+
+    if (containerKeys.length > 0) {
+      for (int i = 0; i < pinLocations.length; i++) {
+        // print(pinLocations[i][0]);
+        stackChildren.add(
+          Positioned(
+            left: pinLocations[i][0],
+            top: MediaQuery.of(context).size.height * 0.05 + pinLocations[i][1],
+            child: Container(
+              decoration: BoxDecoration(
+                // color: Colors.black,
+              ),
+              key: containerKeys[i],
+              height: 30,
+              width: 30,
+            ),
+          ),
+        );
+
+        pins.add(
+          Positioned(
+            top: (topStart[i] == null) ? 0 : (topStart[i] * 1.0) - 40,
+            left: (leftStart[i] == null) ? 0 : leftStart[i] * 1.0,
+            child: InkWell(
+              onTap: () {
+                final sectionKey = SectionKey(id: i);
+                  context.provide<AppNotifier>(listen: false).push(
+                  context: context,
+                  routeInfo: RouteInfo(
+                    name: sectionNames[i],
+                    dataKey: sectionKey,
+                    route: CrossFadePageRoute(
+                      builder: (context) {
+                        return SectionDetailsPage(
+                          sectionKey: sectionKey,
+                        );
+                      },
+                    ),
+                  ),
+                );
+              },
+              child: Container(
+                height: 40,
+                child: Row(
+                  children: [
+                    // Image(image: AssetImage("assets/images/pin.png")),
+                    Container(
+                      padding: EdgeInsets.all(4.0),
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.all(Radius.circular(4.0)),
+                        color: Color.fromARGB(172, 255, 255, 255),
+                      ),
+                      child: Text(sectionNames[i], style: TextStyle(fontSize: 12.0)),
+                    ),
+                  ],
+                )
+              ),
+            )
+          ),
+        );
+      }
+    }
+    else {
+      setState(() {
+        for (int i = 0; i < pinLocations.length; i++) {
+          containerKeys.add(GlobalKey());
+        }
+      });
+    }
+
     return CustomAnimatedSwitcher(
       fadeIn: true,
       child: SafeArea(
-          child: InteractiveViewer(
+        child: Stack(
+          children: [
+            InteractiveViewer(
+              onInteractionEnd: (ScaleEndDetails details) {
+                updatePositions();
+              },
+              onInteractionUpdate: (ScaleUpdateDetails details) {
+                updatePositions();
+              },
               panEnabled: true,
-              minScale: 1.0,
-              maxScale: 2,
+              minScale: 1.5,
+              maxScale: 2.5,
               child: Container(
                 height: MediaQuery.of(context).size.height,
                 width: MediaQuery.of(context).size.width,
-                decoration: BoxDecoration(
-                    image: DecorationImage(
-                        fit: BoxFit.contain,
-                        image: AssetImage('assets/images/map.png'),
-                        alignment: Alignment.topCenter),
-                    borderRadius: BorderRadius.all(Radius.circular(20))),
-              ))),
+                child: Stack(
+                  children: stackChildren,
+                ),
+              ),
+            ),
+            ...pins
+          ],
+        ),
+      ),
     );
   }
 }
